786e35ba122394eae6fcb568ace1816b471fbd56
Quintus <dangquynh.1412@gmail.com>
2020-07-08 19:43:50 +0700

    message 1

--- src/Selenium_api/Topic_01_Check_Evironment.java
+++ src/Selenium_api/Topic_01_Check_Evironment.java
@@ -2,6 +2,7 @@ package Selenium_api;
 
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
@@ -32,32 +33,38 @@ public class Topic_01_Check_Evironment {
 		 driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		 //maximize browser len
 		 driver.manage().window().maximize();
+		 
 		 //get URL cua web
 		 driver.get("http://live.demoguru99.com/");
 		 
 	  }	
+		@Test
+		 public void TC_01_CheckTitle() {
+			 //lay title cua page
+			 String homePageTitle = driver.getTitle();
+			 //so sanh title co giong nhau khong?
+			 Assert.assertEquals("Home page", homePageTitle);
+		  
+		  
+		  }
 	 
+	  
+		 
+		@Test
+		  public void TC_02_CheckURL() {
+			// check URL cua page
+			 String homePageURL = driver.getCurrentUrl();
+			 Assert.assertEquals("http://live.demoguru99.com/", homePageURL);
+		  }
+		
+		
+		  @AfterClass
+		  public void afterClass() {
+			  //quit browser
+			  driver.quit();
+		  }
 	 
   
-	 
-	@Test
-	 public void TC_01_CheckTitle() {
-		 
-	  
-	  
-	  }
- 
-  
-	 
-	@Test
-	  public void TC_02_CheckURL() {
-		 
-	  }
 	
-	  @AfterClass
-	  public void afterClass() {
-		  //quit browser
-		  driver.quit();
-	  }
 
 }
